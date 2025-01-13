618ec8e59828dc342d117e0fbe4209ff49500de9
Michael <90670740+MJhin0@users.noreply.github.com>
2025-01-13 11:18:22 -0500

    Archiving Old Formats
    The college website and the new navigation bar, main will have the new personal website.

---
+++ old/simpleForm.php
@@ -0,0 +1,23 @@
+<?php include 'header.php';  ?>
+<form method="POST" action="processFrom.php">
+    <h3>A Simple Form</h3>
+    <p>First Name <br>
+    <input type="text" name="fname">
+</p>
+<p>Last Name <br>
+    <input type="text" name="lname">
+</p>
+<p>Univesity Status<br>
+<select name="year">
+    <option>Select</option>
+    <option>1st year</option>
+    <option>2nd year</option>
+    <option>3rd year</option>
+    <option>4th year</option>
+</select>
+</p>
+<input type="submit">
+</form>
+
+
+<p> <href= >Student Info</href>
